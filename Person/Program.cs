dbc21ed29cdfbac5d8ea0c09ef5172ed8e4dfb3d
Fozia Abako <foozia.kali@gmail.com>
2025-05-01 00:35:27 +0300

    Added an attribute to the code

--- Person/Program.cs
+++ Person/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            Console.Writeline("Enter Your Name");
+            string Fname = Console.ReadLine();
+
+            Console.WriteLine("Enter Your FatherName");
+            String LName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Age");
+            String Age = Console.ReadLine();
+            
             Console.WriteLine("Hello, World!");
         }
     }
